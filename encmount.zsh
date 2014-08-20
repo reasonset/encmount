2aa193889ee832802a3958952edf0c6fd752850a
MASAKI Yuhsuke <aki@reasonset.net>
2014-08-20 10:54:07 +0900

    Add error check.

--- encmount.zsh
+++ encmount.zsh
@@ -1,9 +1,17 @@
 #!/usr/bin/zsh
 
+# This script takes argument as name.
+if [[ -z $1 ]]
+then
+  print "You need give mount name." &> 2
+  exit1
+fi
+
 # This script needs encfs
 if ! type encfs > /dev/null
 then
   print "Encfs is no found." &> 2
+  exit 1
 fi
 
 typeset -A encmap # name, path, mountpoint mapping assoc.
@@ -20,6 +28,13 @@ while read name epath dest
 do
   encmap[$name]="$epath::$dest"
 done < ~/.yek/encmap
+
+# Abort if the name is not exit.
+if [[ -z encmap[$name] ]]
+then
+  print "$name is not exit in encmap file. Abort." &>2
+  exit 1
+fi
 
 # Decryption.
 encfs ${encmap[$1]%::*} ${encmap[$1]#*::}
